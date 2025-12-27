a9382b1450abc542346870acf8b99d5458ad8da0
RogerJackD <rogerzapata114@gmail.com>
2025-12-26 20:19:40 -0500

    feat(dev): enable UI flow using mock data for testing

---
+++ src/data/dataFamilyProducts.ts
@@ -0,0 +1,44 @@
+import { FamilyProduct } from "../types/familyProducts";
+
+export const exampleDataFamilyProducts: FamilyProduct[] = [
+    {
+        idFamiliaProducto: 1,
+        nombreFamilia: "BEBIDAS"
+    },
+    {
+        idFamiliaProducto: 2,
+        nombreFamilia: "EXTRAS"
+    },
+    {
+        idFamiliaProducto: 3,
+        nombreFamilia: "Category 3"
+    },
+    {   
+        idFamiliaProducto: 4,
+        nombreFamilia: "Category 4"
+    },
+    {
+        idFamiliaProducto: 5,
+        nombreFamilia: "Category 5"
+    },
+    {
+        idFamiliaProducto: 6,
+        nombreFamilia: "Category 6"
+    },
+    {
+        idFamiliaProducto: 7,
+        nombreFamilia: "Category 7"
+    },
+    {
+        idFamiliaProducto: 8,
+        nombreFamilia: "Category 8"
+    },
+    {
+        idFamiliaProducto: 9,
+        nombreFamilia: "Category 9"
+    },
+    {
+        idFamiliaProducto: 10,
+        nombreFamilia: "Category 10"
+    }
+];
---
+++ src/data/dataproducts.ts
@@ -0,0 +1,74 @@
+import { Product } from "../types/product";
+
+export const exampleDataProducts: Product[] = [
+    {
+        idProducto: 1,
+        nombre: "Product A",
+        familiaProducto : "BEBIDAS",
+        precio: 29.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 2,
+        nombre: "Product B",
+        familiaProducto : "Category 2",
+        precio: 49.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 3,
+        nombre: "Product C",
+        familiaProducto : "Category 3",
+        precio: 59.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 4,
+        nombre: "Product D",
+        familiaProducto : "EXTRAS",
+        precio: 69.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 5,
+        nombre: "Product E",
+        familiaProducto : "BEBIDAS",
+        precio: 79.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 6,
+        nombre: "Product F",
+        familiaProducto : "Category 6",
+        precio: 89.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 7,
+        nombre: "Product G",
+        familiaProducto : "Category 7",
+        precio: 99.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 8,
+        nombre: "Product H",
+        familiaProducto : "EXTRAS",
+        precio: 109.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 9,
+        nombre: "Product I",
+        familiaProducto : "Category 9",
+        precio: 119.99,
+        foto: "product-a.jpg"
+    },
+    {
+        idProducto: 10,
+        nombre: "Product J",
+        familiaProducto : "EXTRAS",
+        precio: 129.99,
+        foto: "product-a.jpg"
+    }
+];
