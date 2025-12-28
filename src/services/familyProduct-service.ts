86d73546c40c066637a5c07334db3389eebf6e47
RogerJackD <rogerzapata114@gmail.com>
2025-12-27 20:24:08 -0500

    feat(services): add service method fetch data familyProducts

--- src/services/familyProduct-service.ts
+++ src/services/familyProduct-service.ts
@@ -1,9 +1,6 @@
 export const familyProductService = {
     getFamilyProducts: async () => {
-        // const response = await fetch('http://localhost:3000/api/products');
-        // return response.json();
-
-        const { exampleDataFamilyProducts } = await import('../data/dataFamilyProducts');
-        return exampleDataFamilyProducts;
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/catalog/families`);
+        return response.json();
     }
 };
