982831b592c06fbc88da4a3654c8313bbdf9c4b8
RogerJackD <rogerzapata114@gmail.com>
2025-12-27 20:27:01 -0500

    feat(services): add service method fetch data getCodeProduct

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -6,5 +6,8 @@ export const productService = {
         return response.json();
     },
 
-    
+    getCodeProduct: async (): Promise<CodeProduct> => {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/catalog/generate-code`);
+        return response.json();
+    } 
 };
